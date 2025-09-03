9b39988bba3f9e902564d8b65b01d9a4052c94f0
Nishantyadav1512 <kumarnishantyadav27@gmail.com>
2025-09-03 06:08:51 +0530

    updated index  file

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ import {app, server } from "./lib/socket.js"
 
 
 const PORT = process.env.PORT;
-const _dirname = path.resolve();
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -26,16 +26,14 @@ app.use(cors({
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
-if(process.env.NODE_ENV ==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
-app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-
-})
-
-connectDB(); // Call the function here
-
 server.listen(PORT, () => {
     console.log(`server is running on PORT: ${PORT}`)
+    connectDB();
 });
